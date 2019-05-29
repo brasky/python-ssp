11f210ec9c30ea7eea1e28fa8980ae8846f02ef3
Elliot DeMatteis <elliotdematteis@gmail.com>
2019-05-28 21:24:51 -0700

    testing travis-ci

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,6 +12,6 @@ from ssp.api import SSP
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def test():
-    path = TEST_DIR + '\\test_files\\test.docx'
+    path = TEST_DIR + '/test_files/test.docx'
     securityplan = SSP(path)
     assert isinstance(securityplan.document, docx.document.Document)
