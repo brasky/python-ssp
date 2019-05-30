23dc38ebcf4d8671f906945596a0afc3bbff7415
Elliot DeMatteis <elliotdematteis@gmail.com>
2019-05-29 19:56:42 -0700

    new tests and fedramp templates

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,11 +7,14 @@ import sys
 import pytest
 import docx
 
+import ssp
 from ssp.api import SSP
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 
-def test():
-    path = TEST_DIR + '/test_files/test.docx'
-    securityplan = SSP(path)
-    assert isinstance(securityplan.document, docx.document.Document)
+class TestAPI(object):
+
+    def test_that_word_doc_opens(self):
+        path = TEST_DIR + '/test_files/test.docx'
+        securityplan = SSP(path)
+        assert isinstance(securityplan, ssp.securityplan.SecurityPlan)
---
+++ tests/test_securityplan.py
@@ -0,0 +1,33 @@
+"""
+Test suite for ssp.securityplan module.
+"""
+import os
+import sys
+
+import pytest
+import docx
+
+from ssp.securityplan import SecurityPlan
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+
+class TestSecurityPlan(object):
+    
+    def test_that_word_doc_opens(self):
+        """
+        Make sure basic word docs open, basically testing docx is working.
+        """
+        path = TEST_DIR + '/test_files/test.docx'
+        securityplan = SecurityPlan(path)
+        assert isinstance(securityplan.document, docx.document.Document)
+    
+    def test_get_version_returns_correct_version(self):
+        """
+        Test that the proper versions are obtained from template
+        """
+        version = '08282018'
+        path = TEST_DIR + '/test_files/blank_templates/' + version + '/'
+        file_name = 'FedRAMP-SSP-High-Baseline-Template.docx'
+
+        securityplan = SecurityPlan(path + file_name)
+        assert securityplan.version == version
