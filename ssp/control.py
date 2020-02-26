74816edd717470d2410fd1d4aa6052abf5a2e70f
Michael DeMichillie (First Information Technology) <v-midemi@microsoft.com>
2020-02-26 14:37:47 -0800

    Fixed handling for single-part controls with exts

--- ssp/control.py
+++ ssp/control.py
@@ -28,6 +28,8 @@ class Control(object):
 
     def get_parts(self):
         self.parts = []
+        if bool(re.match(r'\w+-\d',self.implementation_table.cell(1,0).text)):
+            self.parts.append('Main')
         for row in self.implementation_table.rows:
             if "Part" in row.cells[0].text and len(row.cells[0].text) < 8:
                 self.parts.append(row.cells[0].text.replace('Part ', '').strip())
@@ -35,8 +37,7 @@ class Control(object):
                 self.parts.append(row.cells[0].text.replace('Req. ', '').strip())
             elif 'Ext.' in row.cells[0].text and len(row.cells[0].text) < 8:
                 self.parts.append(row.cells[0].text.strip())
-            elif bool(re.match(r'\w+-\d',row.cells[0].text)):
-                self.parts.append('Main')
+                
 
         if len(self.parts) < 1 and len(self.implementation_table.rows) < 3:
             self.parts.append(None)
@@ -65,6 +66,12 @@ class Control(object):
                     return self.implementation_table.cell(self.LETTERS[letter]+row_difference, 1)
                 except:
                     ValueError('Control does not have part ' + part_id)
+            elif bool(re.match(r'\w{3,}', part_id)):
+                try:
+                    row = [cell.text.strip() for cell in self.implementation_table.columns[0].cells].index(part_id)
+                    return self.implementation_table.cell(row, 1)
+                except ValueError:
+                    raise ValueError('Control does not have part ' + part_id)
             else:
                 try:
                     return self.implementation_table.cell(self.LETTERS[part_id], 1)
